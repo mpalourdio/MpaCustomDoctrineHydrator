fadaaef8e51f61889d79fad39e7acfdf97ffd10a
mpalourdio <mpalourdio@gmail.com>
2013-10-03 16:28:03 +0200

    throw exception on bad parameter received and year/month formatted to be compliant with almost all js datepicker

--- src/CustomDoctrineHydrator/Stdlib/Hydrator/Strategy/DateToStringStrategy.php
+++ src/CustomDoctrineHydrator/Stdlib/Hydrator/Strategy/DateToStringStrategy.php
@@ -20,8 +20,13 @@ class DateToStringStrategy implements StrategyInterface
     public function extract($value)
     {
         /** @var $value DateTime */
-        if (!is_null($value) && $value instanceof DateTime)
-        {
+        if (!is_null($value)) {
+            if (!($value instanceof DateTime)) {
+                throw new \InvalidArgumentException(sprintf(
+                        'Field "%s" is not a valid DateTime object',
+                        $value)
+                );
+            }
             $fmt = new IntlDateFormatter(
                 Locale::getDefault(),
                 IntlDateFormatter::SHORT,
@@ -30,9 +35,20 @@ class DateToStringStrategy implements StrategyInterface
                 IntlDateFormatter::GREGORIAN
             );
 
+            //we want years to always be 4 chars when pattern has only 2 y or 2 Y
+            //substr_count is case sensitive
+            if (substr_count(strtolower($fmt->getPattern()), "y") === 2) {
+                $fmt->setPattern(str_ireplace('y', 'yy', $fmt->getPattern()));
+            }
+
+            //we want days to always be 2 chars when pattern has only 1 d
+            if (substr_count($fmt->getPattern(), "d") === 1) {
+                $fmt->setPattern(str_ireplace('d', 'dd', $fmt->getPattern()));
+            }
+
             return $fmt->format($value);
         } else {
-            return null;
+            return $value;
         }
     }
 
