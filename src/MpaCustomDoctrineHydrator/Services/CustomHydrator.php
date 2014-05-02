ee51a6be37f06f5ba1bbb46789f5762ecc2d7f4c
mpalourdio <mpalourdio@gmail.com>
2014-05-03 01:35:43 +0200

    refactored to better inject dependencies

---
+++ src/MpaCustomDoctrineHydrator/Services/CustomHydrator.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+namespace MpaCustomDoctrineHydrator\Services;
+
+use Doctrine\ORM\EntityManagerInterface;
+use DoctrineModule\Stdlib\Hydrator\DoctrineObject as DoctrineHydrator;
+use MpaCustomDoctrineHydrator\Stdlib\Hydrator\Strategy\DateToStringStrategy;
+
+class CustomHydrator
+{
+    protected $entityManager;
+    protected $dateConfig;
+
+    /**
+     * @param EntityManagerInterface $entityManager
+     * @param array $dateConfig
+     */
+
+    public function __construct(EntityManagerInterface $entityManager, array $dateConfig)
+    {
+        $this->entityManager = $entityManager;
+        $this->dateConfig    = $dateConfig;
+    }
+
+    /**
+     * @param string $entity
+     * @return DoctrineHydrator
+     */
+    public function setEntity($entity)
+    {
+        $hydrator = new DoctrineHydrator($this->entityManager);
+
+        $columns = $this->entityManager->getClassMetadata($entity)->getColumnNames();
+        foreach ($columns as $column) {
+            $type = $this->entityManager->getClassMetadata($entity)->getTypeOfColumn($column);
+            if ('date' === $type) {
+                $hydrator->addStrategy($column, new DateToStringStrategy($this->dateConfig));
+            }
+        }
+
+        return $hydrator;
+    }
+}
