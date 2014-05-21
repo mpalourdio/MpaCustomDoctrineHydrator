99c719b18cfafd6edb78caeaf19f475e7071f40b
mpalourdio <mpalourdio@gmail.com>
2014-05-21 08:42:08 +0200

    factory for time element filter

---
+++ src/MpaCustomDoctrineHydrator/Factory/TimeToDateTimeFilterFactory.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+namespace MpaCustomDoctrineHydrator\Factory;
+
+use Locale;
+use MpaCustomDoctrineHydrator\Filter\TimeToDateTime;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class TimeToDateTimeFilterFactory implements FactoryInterface
+{
+    /**
+     * Create service
+     *
+     * @param  ServiceLocatorInterface $serviceLocator
+     * @return TimeToDateTime
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $parentLocator = $serviceLocator->getServiceLocator();
+
+        $filter = new TimeToDateTime(
+            $parentLocator->get('Config')['mpacustomdoctrinehydrator']['formats'][Locale::getDefault()]
+        );
+
+        return $filter;
+    }
+}
